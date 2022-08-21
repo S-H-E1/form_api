6415dded7df5b85ac69632de620a2fba25cdbb2a
S-H-E1 <jasksonshe666999@gmail.com>
2022-08-21 10:57:44 +0300

    progfile

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    def __repr__(self):
+        return self.username
+
+
+@app.route("/", methods= ["POST"] )
+def main():
+    if request.method == "POST":
+        id = request.form.get('id')
+        password = request.form.get("password")
+        user = User(username=id, password=password)
+        db.session.add(user)
+        db.session.commit()
+        data = User.query.all()
+        print(data)
+        return "<p> this is awsome</p>"
+
+@app.route("/random")
+def random():
+    users = User.query.all()
+    return render_template('users.html', users=users)
